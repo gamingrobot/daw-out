6c4b30a59d332455bf8adb72b3b2e26aa51b711f
Morgan Creekmore <morgan@creekmore.email>
2022-07-20 23:38:32 -0500

    Removed unused code

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
-use nih_plug::param;
-use nih_plug::prelude::{Editor, Param};
+
+use nih_plug::prelude::Editor;
 use nih_plug_vizia::vizia::prelude::*;
 use nih_plug_vizia::widgets::*;
 use nih_plug_vizia::{assets, create_vizia_editor, ViziaState};
@@ -10,8 +10,6 @@ use crate::DawOutParams;
 
 /// VIZIA uses points instead of pixels for text
 const POINT_SCALE: f32 = 0.75;
-
-const STYLE: &str = r#""#;
 
 #[derive(Lens)]
 struct DawOutEditor {
@@ -30,7 +28,7 @@ pub(crate) fn create(
     editor_state: Arc<ViziaState>,
 ) -> Option<Box<dyn Editor>> {
     create_vizia_editor(editor_state, move |cx, _| {
-        cx.add_theme(STYLE);
+        //cx.add_theme(STYLE);
 
         DawOutEditor {
             params: params.clone(),
@@ -77,31 +75,5 @@ pub(crate) fn create(
         .row_between(Pixels(0.0))
         .child_left(Stretch(1.0))
         .child_right(Stretch(1.0));
-
-        // VStack::new(cx, |cx| {
-        //     Label::new(cx, "Server Address").bottom(Pixels(-1.0));
-        //     Label::new(cx, "Server Port").bottom(Pixels(-1.0));
-        //     Label::new(cx, "OSC Address Base").bottom(Pixels(-1.0));
-
-        //     Label::new(cx, "param1").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param1);
-        //     Label::new(cx, "param2").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param2);
-        //     Label::new(cx, "param3").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param3);
-        //     Label::new(cx, "param4").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param4);
-        //     Label::new(cx, "param5").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param5);
-        //     Label::new(cx, "param6").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param6);
-        //     Label::new(cx, "param7").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param7);
-        //     Label::new(cx, "param8").bottom(Pixels(-1.0));
-        //     ParamSlider::new(cx, Data::params, |params| &params.param8);
-        // })
-        // .row_between(Pixels(0.0))
-        // .child_left(Stretch(1.0))
-        // .child_right(Stretch(1.0));
     })
 }
